831f2e2a5adec6aa4e6fa8ec3564b1ceca002f74
swiftyirons <135535040+swiftyirons@users.noreply.github.com>
2023-08-01 19:11:14 -0500

    Add function to play a game round

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
   switch(randomNumber) {
     case 1: 
       return "rock";
@@ -12,4 +12,19 @@ function getComputerChoice() {
       break;
   }
 }
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+
+  playerSelection = playerSelection.toLowerCase();
+  const won = (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper");
+  const lost = (playerSelection === "scissors" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors");
+
+  if (won) {
+    return `You won! ${playerSelection} beats ${computerSelection}`;
+  } else if (lost) {
+    return `You lost! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `You tied. You both chose ${playerSelection}`
+  }
+}
+
+console.log(playRound("Scissors", getComputerChoice()));

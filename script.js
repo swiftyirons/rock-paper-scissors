9533262766055d787b35277253321d46bb108068
swiftyirons <135535040+swiftyirons@users.noreply.github.com>
2023-08-01 23:09:36 -0500

    Add function to play 5 round game

--- script.js
+++ script.js
@@ -27,4 +27,29 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-console.log(playRound("Scissors", getComputerChoice()));
+function declareWinner(gamesWon, gamesLost) {
+  if (gamesWon > gamesLost) {
+    return `You are the Winner! You won ${gamesWon} and lost ${gamesLost} games.`;
+  } else if (gamesWon < gamesLost) {
+    return `You are the Loser! You won ${gamesWon} and lost ${gamesLost} games.`;
+  } else {
+    return `You tied. You won ${gamesWon} and lost ${gamesLost} games.`
+  }
+}
+
+function game() {
+  let gamesWon = 0;
+  let gamesLost = 0;
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = prompt("Please enter rock, paper, or scissors: ");
+    let result = playRound(playerSelection, getComputerChoice());
+    console.log(result);
+    if (result.charAt(4) === "w") {
+      gamesWon +=1;
+    } else if (result.charAt(4) === "l") {
+      gamesLost += 1;
+    }
+  }
+  console.log(declareWinner(gamesWon, gamesLost));
+} 
+game();
